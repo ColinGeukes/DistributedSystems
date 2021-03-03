1e3f733acc066bc08c5940a8d8ce0fa099e365e6
matthijsr <matthijsr@users.noreply.github.com>
2021-03-03 12:22:33 +0100

    Try scrypto (#1)
    * Try out/import SCrypto
    
    * More .gitignore changes
    
    * build.sbt and more .gitignore changes

--- build.sbt
+++ build.sbt
@@ -1,5 +1,13 @@
-name := "DistributedSystems2"
-
+name := "GRuB_Scala"
 version := "0.1"
+description := "GRuB for Scala"
 
-scalaVersion := "2.12.4"
+scalaVersion := "2.12.12"
+
+resolvers ++= Seq(
+  "Sonatype Releases" at "https://oss.sonatype.org/content/repositories/releases/",
+)
+
+libraryDependencies ++= Seq(
+  "org.scorexfoundation" %% "scrypto" % "2.1.10",
+)
--- src/main/scala/distributeds/systems/HelloWorld.scala
+++ src/main/scala/distributeds/systems/HelloWorld.scala
@@ -1,5 +1,21 @@
 package distributeds.systems
 
+import scorex.crypto.authds.LeafData
+import scorex.crypto.authds.merkle.{Leaf, MerkleTree}
+import scorex.crypto.hash.Keccak256
+
 object HelloWorld extends App {
+  implicit val hf = Keccak256
+
   println("Hello, World!")
+  val d = (0 until 10).map(_ => LeafData @@ scorex.utils.Random.randomBytes(32))
+  val leafs = d.map(data => Leaf(data))
+  val tree = MerkleTree(d)
+  leafs.foreach { l =>
+    val proof = tree.proofByElement(l).get
+    println(s"${proof.leafData}==${l.data}")
+    println(proof.leafData == l.data)
+    println(s"rootHash: ${tree.rootHash}")
+    println(proof.valid(tree.rootHash))
+  }
 }
