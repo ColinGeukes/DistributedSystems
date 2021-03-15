275426ff8ba0089efbf3f86730d524249707a562
Matthijs Reukers <matthijsr@users.noreply.github.com>
2021-03-15 09:16:54 +0100

    First simplify Scala further, implementation without classes.

--- src/main/scala/sgrub/playground/MinimalVerifier.scala
+++ src/main/scala/sgrub/playground/MinimalVerifier.scala
@@ -23,49 +23,6 @@ class MinimalVerifier(latestDigest: Array[Byte]) {
     hash(prefix +: toHash.flatMap(_.toList).toArray)
   }
 
-  // Just renaming/simplifying these ones to keep track
-  trait BasicNode {
-    var visited = false
-    // Array of 32 bytes, making up the hash/digest
-    def label: Array[Byte]
-  }
-
-  class ImpNode(l: Array[Byte]) extends BasicNode {
-    override def label: Array[Byte] = l
-  }
-
-  trait HasKey {
-    // Array of size KeyLength (length = 8 => uint256!)
-    var key: Array[Byte]
-  }
-
-  // I sure hope Solidity supports inheritance of structs/classes...
-  trait ProperLeaf extends BasicNode with HasKey {
-    def value: Array[Byte]
-    // Array of size KeyLength (length = 8 => uint256!)
-    def nextLeafKey : Array[Byte]
-    override def label: Array[Byte] = prefixedHash(0: Byte, key, value, nextLeafKey)
-  }
-
-  class ImpLeaf(k: Array[Byte], v: Array[Byte], nk: Array[Byte]) extends ProperLeaf {
-    override def value: Array[Byte] = v
-    override def nextLeafKey: Array[Byte] = nk
-    var key: Array[Byte] = k
-  }
-
-  trait InternalNode extends BasicNode {
-    def balance: Byte
-    def left: BasicNode
-    def right: BasicNode
-    override val label: Array[Byte] = prefixedHash(1: Byte, Array(balance), left.label, right.label)
-  }
-
-  class ImpInternalNode(b: Byte, l: BasicNode, r: BasicNode) extends InternalNode {
-    override def balance: Byte = b
-    override def left: BasicNode = l
-    override def right: BasicNode = r
-  }
-
   // This trait already exists in the package, I'm just copying it here to keep track of what's actually happening
   // Note that Byte acts as int8
   // Do not use bytes -1, 0, or 1 -- these are for balance
@@ -83,7 +40,7 @@ class MinimalVerifier(latestDigest: Array[Byte]) {
       var value: Option[Array[Byte]] = None
       val maxNodes = 2 * rootNodeHeight + 2
       var numNodes = 0
-      var s = List.empty[BasicNode] // Nodes and depths
+      var s = List.empty[Array[Byte]] // Nodes and depths
       var i = 0
       while (proof(i) != EndOfTreeInPackagedProof) {
         val n = proof(i)
@@ -98,7 +55,7 @@ class MinimalVerifier(latestDigest: Array[Byte]) {
         if (n == LabelInPackagedProof) {
           val label = proof.slice(i, i + labelLength)
           i += labelLength
-          s = new ImpNode(label) +: s
+          s = label +: s
         }
         else if (n == LeafInPackagedProof) {
           // There should only be one of these...
@@ -116,23 +73,24 @@ class MinimalVerifier(latestDigest: Array[Byte]) {
           val nextLeafKey = proof.slice(i, i + keyLength)
           i += keyLength
           val valueLength = Ints.fromByteArray(proof.slice(i, i + 4))
-          i +=4
+          i += 4
           value = Some(proof.slice(i, i + valueLength))
           i += valueLength
-          val leaf = new ImpLeaf(leafKey, value.get, nextLeafKey)
-          s = leaf +: s
+          val label = prefixedHash(0: Byte, leafKey, value.get, nextLeafKey)
+          s = label +: s
         }
         else {
           val right = s.head
           s = s.tail
           val left = s.head
           s = s.tail
-          s = new ImpInternalNode(n, left, right) +: s
+          val label = prefixedHash(1: Byte, Array(n), left, right)
+          s = label +: s
         }
       }
       if (s.size == 1) {
         val root = s.head
-        if (latestDigest.sameElements(root.label)) {
+        if (latestDigest.sameElements(root)) {
           callback(value)
           return true
         } else {
